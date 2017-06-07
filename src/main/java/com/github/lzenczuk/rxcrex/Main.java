2f5129ac5a477b0a5d8ed9b2364133f916d34e89
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2017-06-07 11:10:01 +0100

    Refactoring and added json

--- src/main/java/com/github/lzenczuk/rxcrex/Main.java
+++ src/main/java/com/github/lzenczuk/rxcrex/Main.java
@@ -2,15 +2,15 @@ package com.github.lzenczuk.rxcrex;
 
 
 import io.netty.handler.codec.http.websocketx.CloseWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PingWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.PongWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
-import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.handler.logging.LogLevel;
 import io.reactivex.netty.RxNetty;
-import rx.Observable;
 import rx.Observer;
 import rx.subjects.PublishSubject;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.nio.charset.Charset;
 
 /**
  * Created by dev on 29/05/17.
@@ -18,17 +18,39 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-
         RxNetty.newWebSocketServerBuilder(8078, connection -> {
             System.out.println("Receive connection");
 
             PublishSubject<Void> closeConnection = PublishSubject.create();
 
-            Observable<WebSocketFrame> inputObservable = connection.getInput();
+            connection.getInput().filter(webSocketFrame -> {
 
-            AtomicInteger counter = new AtomicInteger();
+                System.out.println("Is final: "+webSocketFrame.isFinalFragment());
 
-            new Observer<String>(){
+                if(webSocketFrame instanceof PingWebSocketFrame){
+                    System.out.println("Ping");
+                    connection.writeAndFlush(new PongWebSocketFrame(webSocketFrame.content().retain()));
+                    return false;
+                }
+
+                if(webSocketFrame instanceof PongWebSocketFrame){
+                    System.out.println("Pong");
+                    return false;
+                }
+
+                if(webSocketFrame instanceof CloseWebSocketFrame){
+                    System.out.println("Close frame");
+                    closeConnection.onCompleted();
+                    return false;
+                }
+
+                return true;
+            }).subscribe(webSocketFrame -> {
+                System.out.println("Receive strings: "+webSocketFrame.content().toString(Charset.defaultCharset()));
+                webSocketFrame.retain();
+            });
+
+            Observer<String> outputObserver = new Observer<String>() {
 
                 @Override
                 public void onCompleted() {
@@ -47,21 +69,6 @@ public class Main {
                     connection.writeAndFlush(new TextWebSocketFrame(s));
                 }
             };
-
-            inputObservable.subscribe(webSocketFrame -> {
-                if(webSocketFrame instanceof CloseWebSocketFrame){
-                    System.out.println("Close web socket frame. Closing connection.");
-                    closeConnection.onCompleted();
-                }else {
-                    System.out.println("Receive " + webSocketFrame.toString());
-                    int n = counter.addAndGet(1);
-                    System.out.println("N = " + n);
-                    if (n > 5) {
-                        System.out.println("Closing connection");
-                        closeConnection.onCompleted();
-                    }
-                }
-            });
 
             return closeConnection;
         }).enableWireLogging(LogLevel.INFO).build().startAndWait();
